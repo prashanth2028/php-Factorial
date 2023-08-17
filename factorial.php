266298fd4e7c491f36e9f128e45ca3665724ce11
prashanth2028 <142397788+prashanth2028@users.noreply.github.com>
2023-08-17 15:17:58 +0530

    Add files via upload

---
+++ factorial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>  
+<form method="post">  
+    Enter the Number:<br>  
+    <input type="text" name="number" id="number">  
+    <input type="submit" name="submit" value="Submit" />  
+</form><br>  
+<?php   
+    if($_POST){  
+        $fact = 1;  
+        //getting value from input text box 'number'  
+        $number = $_POST['number'];  
+        echo "Factorial of $number:<br><br>";  
+        //start loop  
+        for ($i = 1; $i <= $number; $i++){         
+            $fact = $fact * $i;  
+            }  
+            echo $fact . "<br>";  
+    }  
+?>  
+</body>  
+</html>  
